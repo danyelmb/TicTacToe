ec7615eb08b50062eface696667b2871dc1327ef
danyelmb <danyel_m_96@hotmail.com>
2018-05-24 03:06:37 +0200

    Casos Unitarios y Dobles Completos
    Falta caso sistema, plantilla hecha

--- tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
+++ tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/BoardTest.java
@@ -3,73 +3,113 @@ package es.codeurjc.ais.tictactoe;
 
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
 public class BoardTest extends TestCase{
-    /*private Board board;
-    private TicTacToeGame tictac;
-    private Player p1;
-    private Player p2;
-    private Connection con1;
-    private Connection con2;
-
+    private Board board;
+    
+    private String lineaJugador(String label, int[] linea){
+        String player;
+        String result;
+        if(label.equals("X")){
+            player = "Player 1 with label " + label;
+        }else{
+            player = "Player 2 with label " + label;
+        }
+        if(linea!=null){
+            result = " should win with line: " + linea[0] + " - " + linea[1] + " - " + linea[2];
+        }else{
+            result = " shouldn't win the game.";
+        }
+        return player + result;
+    }
     
     
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    private void turnoJugador(String label, int celda){
+        board.getCell(celda).active=true;
+        board.getCell(celda).value=label;
+    }
+    
+    /**
+     *
+     * @throws Exception
+     */
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        board = new Board();   
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    /**
+     *
+     * @throws Exception
+     */
+    @After
+    @Override
+    public void tearDown() throws Exception {
+        board = null;
+    }
 
-	@Before
-	public void setUp() throws Exception {
-            tictac = new TicTacToeGame();
-            board = new Board();   
-	}
+    @Test
+    public void testGetCellsIfWinnerP1() {
 
-        @After
-	public void tearDown() throws Exception {
-            board = null;
-            tictac = null;
-	}
-        */
-	@Test
-	public void testGetCellsIfWinnerP1() {
-            Board board = new Board();
-            board.getCell(0).active=true;
-            board.getCell(0).value="X";
-            board.getCell(3).active=true;
-            board.getCell(3).value="O";
-            board.getCell(1).active=true;
-            board.getCell(1).value="X";
-            board.getCell(4).active=true;
-            board.getCell(4).value="O"; 
-            board.getCell(2).active=true;
-            board.getCell(2).value="X";
+        turnoJugador("X",0); //P1 T1 
+        turnoJugador("O",3); //P2 T2 
+        turnoJugador("X",1); //P1 T3 
+        turnoJugador("O",4); //P2 T4 
+        turnoJugador("X",2); //P1 T5 
+        //Jugador 1 Gana
+        int[] lineaGanadora = {0,1,2};
 
-            int[] lineaGanadora = {0,1,2};
-            
-            assertArrayEquals(board.getCellsIfWinner("X"),lineaGanadora);
-            
-	}
+        assertArrayEquals(lineaJugador("X",lineaGanadora),
+                board.getCellsIfWinner("X"),lineaGanadora);
+        assertArrayEquals(lineaJugador("O",null),
+                board.getCellsIfWinner("O"),null);
+        assertFalse("Game not draw",board.checkDraw());
+    }
 
-	@Test
-	public void testCheckDraw() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetCellsIfWinnerP2() {
+        
+        turnoJugador("X",0); //P1 T1 
+        turnoJugador("O",7); //P2 T2  
+        turnoJugador("X",3); //P1 T3 
+        turnoJugador("O",6); //P2 T4 
+        turnoJugador("X",8); //P1 T5 
+        turnoJugador("O",4); //P2 T6 
+        turnoJugador("X",1); //P1 T7 
+        turnoJugador("O",2); //P2 T8 
+        //Jugador 2 Gana
+        int[] lineaGanadora = {6,4,2};
 
-        @Test
-	public void testGetCellsIfWinnerP2() {
-                fail("Not yet implemented");
-            
-                
-	}
+        assertArrayEquals(lineaJugador("X",null),
+                board.getCellsIfWinner("X"),null);
+        assertArrayEquals(lineaJugador("O",lineaGanadora),
+                board.getCellsIfWinner("O"),lineaGanadora);
+        assertFalse("Game not draw",board.checkDraw());
+    }
+
+    @Test
+    public void testCheckDraw() {
+        
+        turnoJugador("X",0); //P1 T1 
+        turnoJugador("O",1); //P2 T2 
+        turnoJugador("X",2); //P1 T3 
+        turnoJugador("O",3); //P2 T4 
+        turnoJugador("X",4); //P1 T5 
+        turnoJugador("O",8); //P2 T6 
+        turnoJugador("X",5); //P1 T7 
+        turnoJugador("O",6); //P2 T8 
+        turnoJugador("X",7); //P1 T9 
+        //Empate
+        assertArrayEquals(lineaJugador("X",null),
+                board.getCellsIfWinner("X"),null);
+        assertArrayEquals(lineaJugador("0",null),
+                board.getCellsIfWinner("O"),null);
+        assertTrue("Game draw!!",board.checkDraw());
+    }
 }
--- tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameDoble.java
+++ tic-tac-toe-enunciado/src/test/java/es/codeurjc/ais/tictactoe/TicTacToeGameDoble.java
@@ -9,6 +9,7 @@ import es.codeurjc.ais.tictactoe.TicTacToeGame.EventType;
 import es.codeurjc.ais.tictactoe.TicTacToeGame.WinnerValue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.After;
@@ -71,14 +72,12 @@ public class TicTacToeGameDoble extends TestCase{
     @Test
     public void testConnection1() {
         verify(connection1, times(2)).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p1,p2)));
-
     }
     
     @Test
     public void testConnection2() {
         verify(connection2, times(2)).sendEvent(eq(EventType.JOIN_GAME), argThat(hasItems(p1,p2)));
     }
-    
     
     @Test
     public void testTurnosWinner1(){ 
@@ -106,11 +105,49 @@ public class TicTacToeGameDoble extends TestCase{
         int[] ganador = {3,4,5};
         //P1 Ganador
         verify(connection1).sendEvent(eq(EventType.GAME_OVER), argument.capture());
-        assertEquals(argument.getValue().player,p1);
-        assertArrayEquals(argument.getValue().pos, ganador);
+        assertEquals("Should win player " + argument.getValue().player.getName(),argument.getValue().player,p1);
+        assertArrayEquals("Cells: "+ Arrays.toString(ganador) ,argument.getValue().pos, ganador);
         verify(connection2).sendEvent(eq(EventType.GAME_OVER), argument.capture());
-        assertEquals(argument.getValue().player,p1);
-        assertArrayEquals(argument.getValue().pos, ganador);
+        assertEquals("Should win player " + p1.getName(),argument.getValue().player,p1);
+        assertArrayEquals("Cells: "+ Arrays.toString(ganador) ,argument.getValue().pos, ganador);
+    }
+    
+    @Test
+    public void testTurnosWinner2(){ 
+        //Turno P1
+        assertTrue(tictac.checkTurn(1));
+        assertFalse(tictac.checkTurn(2));
+        tictac.mark(4);
+        //Turno P2
+        assertFalse(tictac.checkTurn(1));
+        assertTrue(tictac.checkTurn(2));
+        tictac.mark(0);
+        //Turno P1
+        assertTrue(tictac.checkTurn(1));
+        assertFalse(tictac.checkTurn(2));
+        tictac.mark(2);
+        //Turno P2
+        assertFalse(tictac.checkTurn(1));
+        assertTrue(tictac.checkTurn(2));
+        tictac.mark(6);
+        //Turno P1
+        assertTrue(tictac.checkTurn(1));
+        assertFalse(tictac.checkTurn(2));
+        tictac.mark(8);
+        //Turno P2
+        assertFalse(tictac.checkTurn(1));
+        assertTrue(tictac.checkTurn(2));
+        tictac.mark(3);
+        
+        //Array Ganador
+        int[] ganador = {0,3,6};
+        //P2 Ganador
+        verify(connection1).sendEvent(eq(EventType.GAME_OVER), argument.capture());
+        assertEquals("Should win player " + argument.getValue().player.getName(),argument.getValue().player,p2);
+        assertArrayEquals("Cells: "+ Arrays.toString(argument.getValue().pos) ,argument.getValue().pos, ganador);
+        verify(connection2).sendEvent(eq(EventType.GAME_OVER), argument.capture());
+        assertEquals("Should win player " + argument.getValue().player.getName(),argument.getValue().player,p2);
+        assertArrayEquals("Cells: "+ Arrays.toString(argument.getValue().pos) ,argument.getValue().pos, ganador);
     }
     
     @Test
@@ -153,6 +190,8 @@ public class TicTacToeGameDoble extends TestCase{
         tictac.mark(8);
         //Draw
         verify(connection1).sendEvent(eq(EventType.GAME_OVER), argument.capture());
-        assertEquals(argument.getValue(),null);
+        assertEquals("Draw!!",argument.getValue(),null);
+        verify(connection1).sendEvent(eq(EventType.GAME_OVER), argument.capture());
+        assertEquals("Draw!!",argument.getValue(),null);
     }
 }
